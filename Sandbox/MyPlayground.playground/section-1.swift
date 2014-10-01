638e0bbe2f941f2fa6d648f23acd55f03584e264
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2014-10-01 11:59:29 -0400

    Added Sandbox Playground Files

---
+++ Sandbox/MyPlayground.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground";
+let maxLoopConstant = 10;
+
+for index in 1...maxLoopConstant{
+    println(index);
+    }
